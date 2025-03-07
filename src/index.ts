f17ad8b60b2489de768390948b897e80523b71df
Ricardo Rivero <ricardo@enzadon.com>
2025-03-07 14:12:57 -0500

    update endpoint

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,9 @@ const app = express();
 app.use(express.json());
 
 app.get("/sse", (req: Request, res: Response): void => {
-  const transport = new SSEServerTransport("/generate-asset", res);
+  const transport = new SSEServerTransport("/messages", res);
+
+  console.log("[ SSE ] called");
   const sessionId = transport.sessionId;
 
   transports.set(sessionId, transport);
@@ -69,34 +71,30 @@ app.get("/sse", (req: Request, res: Response): void => {
   });
 });
 
-app.post(
-  "/generate-asset",
-  async (req: Request, res: Response): Promise<void> => {
-    const sessionId = req.query.sessionId as string;
-    if (!sessionId) {
-      res.status(400).json({ error: "sessionId query parameter is required" });
-      return;
-    }
-
-    const transport = transports.get(sessionId);
-    if (!transport) {
-      res.status(400).json({
-        error:
-          "No active SSE connection found for this session. Please connect to /sse first",
-      });
-      return;
-    }
-
-    try {
-      return await transport.handlePostMessage(req, res, req.body);
-    } catch (error: any) {
-      console.error("Error stack:", error.stack);
-      res
-        .status(500)
-        .json({ error: error.message || "Unknown error occurred" });
-    }
+app.post("/messages", async (req: Request, res: Response): Promise<void> => {
+  console.log("[ Messages ] called");
+  const sessionId = req.query.sessionId as string;
+  if (!sessionId) {
+    res.status(400).json({ error: "sessionId query parameter is required" });
+    return;
   }
-);
+
+  const transport = transports.get(sessionId);
+  if (!transport) {
+    res.status(400).json({
+      error:
+        "No active SSE connection found for this session. Please connect to /sse first",
+    });
+    return;
+  }
+
+  try {
+    return await transport.handlePostMessage(req, res, req.body);
+  } catch (error: any) {
+    console.error("Error stack:", error.stack);
+    res.status(500).json({ error: error.message || "Unknown error occurred" });
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server running ‼️");
